1776ea0595e42210dd970e7838b888549269db67
Aditya-Paul-2003 <pauladitya936@gmail.com>
2025-02-04 18:44:59 +0530

    connected to Clerk, added favicon and tab icon and added images too

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,18 @@
-function Topbar() {
-    return <h1>Topbar</h1>
-}
+import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
+import { Organization } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
 
-export default Topbar;  
+function Topbar() {
+    const isUserLoggedIn= true;
+    return(
+        <nav className="topbar">
+            <Link href="/" className="flex items-center gap-4">
+                <Image src="/assets/logo.svg" alt="logo" width={28} height={28} />
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">Threads</p>
+            </Link>
+            
+        </nav>
+    )
+}
+export default Topbar;
